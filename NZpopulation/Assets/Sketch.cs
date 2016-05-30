29edba3e3af9df9cedd5aa45fe0cf3c09bfc7aa8
CharlotteAbbott <charlotteabbott@wifi-172-23-42-158.uoa-wifi.auckland.ac.nz>
2016-05-30 12:17:12 +1200

    Last project

--- NZpopulation/Assets/Sketch.cs
+++ NZpopulation/Assets/Sketch.cs
@@ -3,7 +3,7 @@ using Pathfinding.Serialization.JsonFx; //make sure you include this using
 
 public class Sketch : MonoBehaviour {
     public GameObject myPrefab;
-    public string _WebsiteURL = "http://{YOUR WEBSITE NAME}.azurewebsites.net/tables/{YOUR TABLE NAME}?zumo-api-version=2.0.0";
+    public string _WebsiteURL = "http://{NZpopulation}.azurewebsites.net/tables/{NZpopulationattempt2}?zumo-api-version=2.0.0";
 
     void Start () {
         //Reguest.GET can be called passing in your ODATA url as a string in the form:
